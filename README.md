7a3c174b54ead2153d39f90d27be3b4ccc033672
Rishi Raj Jain <rjain@llnw.com>
2022-12-27 13:36:25 +0530

    return explicit messages

---
+++ README.md
@@ -0,0 +1,32 @@
+<p align="center">
+    <h1 align="center">
+        TTFB Booster
+    </h1>
+</p>
+
+<p align="center">
+  <a href="https://twitter.com/rishi_raj_jain_">
+    <img src="https://img.shields.io/twitter/follow/rishi_raj_jain_?style=flat&label=%40rishi_raj_jain_&logo=twitter&color=0bf&logoColor=fff" alt="Twitter" />
+  </a>
+</p>
+
+<p align="center">
+  <a href="#introduction"><strong>Introduction</strong></a>·
+  <a href="#deploy-your-own"><strong>Deploy</strong></a>·
+  <a href="#tech-stack"><strong>Tech Stack</strong></a>·
+  <a href="#author"><strong>Author</strong></a>
+</p>
+
+## Introduction
+
+A service that digests `paths` and hits the SpeedVitals API to visit that url around the globe. Comes in handy with deployment platform like [Edgio](https://edg.io) where edge caches allow you to speed up your site without hurting origin bandwidth.
+
+## Tech Stack
+
+- [Express](https://expressjs.com) – framework
+- [SpeedVitals](https://speedvitals.com) – api
+- [Fly](https://fly.io) – hosting
+
+## Author
+
+- Rishi Raj Jain ([@rishi_raj_jain_](https://twitter.com/rishi_raj_jain_))
--- server.js
+++ server.js
@@ -1,32 +1,49 @@
+// Load the env variables
 require('dotenv').config()
 
+// Set up the express app
 const express = require('express')
 const app = express()
 
 // For parsing application/json
 app.use(express.json())
 
+// CommonJS node-fetch
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
+/**
+ * A function that hits the SpeedVitals API
+ * @param path the path on the baseURL to hit
+ * @param baseURL the origin of the paths to be hit
+ * @returns void
+ */
 const speedTestTTFB = async (path, baseURL) => {
-  let regions = ['asia', 'america', 'europe']
-  for (const region of regions) {
-    console.log(`[Boosting] URL: ${baseURL}${path}, Region: ${region}`)
-    const resp = await fetch('https://api.speedvitals.com/v1/ttfb-tests', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-API-KEY': process.env.SPEED_VITALS_KEY,
-      },
-      body: JSON.stringify({ url: `${baseURL}${path}`, region }),
-    })
-    if (resp.ok) {
-      const data = await resp.json()
-      console.log('Status:', resp.status)
+  if (path && baseURL && path.length > 0 && baseURL.length > 0) {
+    let regions = ['asia', 'america', 'europe']
+    for (const region of regions) {
+      console.log(`[Boosting] URL: ${baseURL}${path}, Region: ${region}`)
+      const resp = await fetch('https://api.speedvitals.com/v1/ttfb-tests', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-KEY': process.env.SPEED_VITALS_KEY,
+        },
+        body: JSON.stringify({ url: `${baseURL}${path}`, region }),
+      })
+      if (resp.ok) {
+        const data = await resp.json()
+        console.log('Status:', resp.status)
+      }
     }
   }
 }
 
+/**
+ * A function that validates paths array passed in the post body
+ * @param paths the list of paths to be hit
+ * @example [ { path: "/home" }, { path: "/about" } ]
+ * @returns Boolean
+ */
 const checkPaths = (paths) => {
   if (paths) {
     try {
@@ -51,10 +68,32 @@ app.post('/', async (req, res) => {
     res.status(200).json({ code: 0, message: 'Method Not Allowed.' })
     return
   }
-  if (req.body && req.body.SPEED_VITALS_KEY === process.env.SPEED_VITALS_KEY && checkPaths(req.body.paths) && req.body.baseURL) {
-    res.status(200).json({ code: 1 })
-    for (const url of req.body.paths) {
-      await speedTestTTFB(url.path, req.body.baseURL)
+  // Check if some body is passed
+  if (req.body) {
+    // Check if SPEED_VITALS_KEY exists
+    if (req.body.SPEED_VITALS_KEY) {
+      // Check if SPEED_VITALS_KEY matches
+      if (req.body.SPEED_VITALS_KEY === process.env.SPEED_VITALS_KEY) {
+        // Check if baseURL exists
+        if (req.body.baseURL && req.body.baseURL.length > 0) {
+          // Check if paths are passed as expected
+          if (checkPaths(req.body.paths)) {
+            // All looks ok, pass code: 1 and run the optimizer
+            res.status(200).json({ code: 1 })
+            for (const url of req.body.paths) {
+              await speedTestTTFB(url.path, req.body.baseURL)
+            }
+          } else {
+            res.status(200).json({ code: 0, message: 'paths does not match the expected format.' })
+          }
+        } else {
+          res.status(200).json({ code: 0, message: 'baseURL is required.' })
+        }
+      } else {
+        res.status(200).json({ code: 0, message: 'SPEED_VITALS_KEY is incorrect.' })
+      }
+    } else {
+      res.status(200).json({ code: 0, message: 'SPEED_VITALS_KEY is required.' })
     }
   } else {
     res.status(200).json({ code: 0, message: 'SPEED_VITALS_KEY, paths and baseURL is required.' })
